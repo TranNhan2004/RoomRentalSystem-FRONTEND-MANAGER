a16283e0838fb272ff778d9de48213ef339d6e98
TranNhan2004 <trannhanlv2004@gmail.com>
2025-02-23 02:03:15 +0700

    Modify objectEquals function 23-02-2025

--- src/lib/client/objectEquals.ts
+++ src/lib/client/objectEquals.ts
@@ -1,13 +1,9 @@
 export const objectEquals = (obj1: Record<string, unknown>, obj2: Record<string, unknown>) => {
   for (const key in obj1) {
-    if (!obj1.hasOwnProperty(key) || !obj2.hasOwnProperty(key)) {
+    if (!Object.hasOwn(obj2, key)) {
       return false;
     }
     
-    if (typeof obj1[key] !== typeof obj2[key]) {
-      return false;
-    }
-
     if (obj1[key] !== obj2[key]) {
       return false;
     }
